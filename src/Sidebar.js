f9bb7c0fd89b05a2a18c3b3071d14f44be8c1bb9
유영빈(집) <kevin018111@gmail.com>
2023-03-13 20:16:28 +0900

    사이드바 순서 변경 및 추가

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -13,8 +13,8 @@ let Sidebar = () => {
                     </header>
                     <ul className="none-font">
                         <li><a href="/main">Homepage</a></li>
-                        <li><a href="/main/generic">Generic</a></li>
-                        <li><a href="/main/element">Elements</a></li>
+                        <li><a href="#Ability">SKILL</a></li>
+                        <li><a href="#PortFolio">PORTFOLIO</a></li>
                         <li>
                             <span className="opener">Submenu</span>
                             <ul>
@@ -24,20 +24,8 @@ let Sidebar = () => {
                                 <li><a href="#">Feugiat Veroeros</a></li>
                             </ul>
                         </li>
-                        <li><a href="#">Etiam Dolore</a></li>
-                        <li><a href="#">Adipiscing</a></li>
-                        <li>
-                            <span className="opener">Another Submenu</span>
-                            <ul>
-                                <li><a href="#">Lorem Dolor</a></li>
-                                <li><a href="#">Ipsum Adipiscing</a></li>
-                                <li><a href="#">Tempus Magna</a></li>
-                                <li><a href="#">Feugiat Veroeros</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Maximus Erat</a></li>
-                        <li><a href="#">Sapien Mauris</a></li>
-                        <li><a href="#">Amet Lacinia</a></li>
+                        <li><a href="/main/generic">Generic</a></li>
+                        <li><a href="/main/element">Elements</a></li>
                     </ul>
                 </nav>
 
