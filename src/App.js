2499723566ce2c9eca97201f2144e40df5253eb9
유영빈 <yuyeongbin@yuyeongbin-ui-MacBookPro.local>
2023-03-13 15:51:03 +0900

    element page add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Main from "./Main";
-import Start from "./Start";
+import Element from "./Element";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <Routes>
-                    {/*<Route path={"/"} element={<Start/>}/>*/}
                     <Route path="/" element={<Main/>}/>
+                    <Route path={"/element"} element={<Element/>}/>
+
                 </Routes>
             </BrowserRouter>
         </div>
